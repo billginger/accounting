6f3a0ce0e941645ba6a054c097f79d5244edcd02
Bill <jfg@163.com>
2017-10-14 18:09:27 +0800

    使用koa-router实现路由

--- web/index.js
+++ web/index.js
@@ -1,9 +1,17 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
-	ctx.response.body = 'Hello Koa!';
+router.get('/', ctx => {
+	ctx.body = '这是首页';
 });
+
+router.get('/login', ctx => {
+	ctx.body = '这是登录页面';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3001);
 console.log('accounting is starting at port 3001');
